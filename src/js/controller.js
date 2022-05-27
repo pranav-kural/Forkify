cc9c122654050fb04995d09419ff95e1a4cc9d06
pranav-kural <pranavkural042@gmail.com>
2022-05-27 15:03:39 -0600

    fetching single recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import { transformObjPropNamesToCamelCase } from './utils';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +13,23 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    // ftech single recipe
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    // retrieve data from response
+    const data = await res.json();
+    // check if request failed
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let recipe = transformObjPropNamesToCamelCase(data.data.recipe);
+
+    console.log('recipe', recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+showRecipe();
